2d52b13be275996e4a90a1a1ad5c28be29ff3969
Luillo <labaroasordo@gmail.com>
2016-12-22 19:09:38 -0600

    Notifications enganched

---
+++ firebase/src/main/java/com/example/firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,28 @@
+package com.example.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Luillo on 22/12/16.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "myLog";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle data payload of FCM messages.
+        Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
+        Log.d(TAG, "FCM Notification Message: " +
+                remoteMessage.getNotification());
+        Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+    }
+
+
+}
